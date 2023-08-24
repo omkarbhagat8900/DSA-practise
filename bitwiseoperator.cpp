783c44dc95e864f12a0bda4e2397132ca3b27073
omkar bhagat <142728705+omkarbhagat8900@users.noreply.github.com>
2023-08-24 22:28:03 +0530

    Add files via upload

---
+++ bitwiseoperator.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std ;
+
+int main (){
+   int a = 4 ;
+    int b = 6 ;
+
+    cout<<" a&b "<< (a&b) <<endl;
+    cout<<" a|b "<<(a|b)<<endl;
+    cout<<" a^b "<<  (a^b )<<endl;
+    cout << " a~b "<< ~a <<endl;
+
+}
+
+/*~~~~~~~~~~~~~ output ~~~~~~~~~~~~~~*/
+ /*a&b 4
+ a|b 6
+ a^b 2
+ a~b -5
+ */
+
+/*----------------------------------------------------------------------------*/
