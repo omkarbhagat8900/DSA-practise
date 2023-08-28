3be33d6acbe4191ff964c09bf7ae82c4901fb4e1
omkar bhagat <142728705+omkarbhagat8900@users.noreply.github.com>
2023-08-28 23:45:07 +0530

    Add files via upload

---
+++ problem.cpp
@@ -0,0 +1,91 @@
+#include<iostream>
+using namespace std ;
+
+// int main(){
+//     int a , b = 1;
+//     a = 10;
+//     if (++a)
+//     cout << b;
+//     else
+//     cout<<++b;
+//}
+
+// int main(){
+//     int n ;
+//     cout<< " input value of n : "<< endl;
+//     cin>>n;
+
+//     for (int i=1; i<=n; i++){
+//         cout<<i << endl;
+//     }
+// }
+
+
+/*~~~~~~~~~~~~~~~ output ~~~~~~~~~~~~~~~~~~~~~~
+input value of n : 
+6
+1
+2
+3
+4
+5
+6*/
+
+
+// int main(){
+//     int n;
+//     cout<<" input value of n :";
+//     cin>>n;
+    
+//     int sum = 0;
+//     for  (int i=1; i<=n ; i++){
+//         sum = sum + i;
+
+//     }
+//     cout<<sum<< endl;
+
+// }
+
+/*~~~~~~~~~~~~~~~~ output ~~~~~~~~~~~~~~
+ input value of n :10
+55
+*/
+
+
+//fibonacci series:
+
+// int main (){
+//     int n;
+//     cout<<"input number of n:"<< endl;
+//     cin >>n;
+
+//     int a = 0;
+//     int b = 1;
+//     cout<< a<< " "<<b << endl;
+
+
+//     for(int i= 1  ; i<=n; i++){
+//          int sum = a+b;
+//         cout<<sum<< endl;
+//         a = b;
+//         b = sum;
+//     }
+// }
+
+
+/*~~~~~~~~~~~~~~~~~~~~ output ~~~~~~~~~~~~
+input number of n:
+10
+0 1
+1
+2
+3
+5
+8
+13
+21
+34
+55
+89
+*/
+
